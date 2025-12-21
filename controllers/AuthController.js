485bf5ecfda9c4a072b7cad627a69c96c6940502
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-21 14:35:41 +0300

    fixed server, controller, and router

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,20 +5,15 @@ const Register = async (req, res) => {
   try {
     // Extracts the necessary fields from the request body
     const { email, password, name } = req.body
-
     // Hashes the provided password
     let passwordDigest = await middleware.hashPassword(password)
-
     // Checks if there has already been a user registered with that email
     let existingUser = await User.exists({ email })
     if (existingUser) {
-
       return res.status(400).send('A user with that email has already been registered!')
     } else {
-
       // Creates a new user
       const user = await User.create({ name, email, passwordDigest })
-
       // Sends the user as a response
       res.status(200).send(user)
     }
@@ -31,10 +26,8 @@ const Login = async (req, res) => {
   try {
     // Extracts the necessary fields from the request body
     const { email, password } = req.body
-
     // Finds a user by a particular field (in this case, email)
     const user = await User.findOne({ email })
-
     // Checks if the password matches the stored digest
     let matched = await middleware.comparePassword(
       password,
@@ -42,7 +35,7 @@ const Login = async (req, res) => {
     )
     // If they match, constructs a payload object of values we want on the front end
     if (matched) {
-      let payload =  {
+      let payload = {
         id: user._id,
         name: user.name,
         email: user.email
@@ -52,7 +45,7 @@ const Login = async (req, res) => {
       return res.status(200).send({ user: payload, token })
     }
     res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
-  } catch (error){
+  } catch (error) {
     console.log(error)
     res.status(401).send({ status: 'Error', msg: 'An error has occurred logging in!' })
   }
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,3 +3,18 @@ const controller = require('../controllers/AuthController')
 const middleware = require('../middleware')
 
 router.post('/register', controller.Register)
+router.post('/login', controller.Login)
+router.put(
+  '/update/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+
+module.exports = router
