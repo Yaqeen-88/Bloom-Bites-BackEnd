145d70c794a5d5bd99f42fb8a035cdb9bd8d7594
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-22 19:33:33 +0300

    intial set up for order model

---
+++ models/Order.js
@@ -0,0 +1,20 @@
+const { Schema, default: mongoose } = require("mongoose")
+
+const orderSchema = new Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  items: [
+    {
+      productType: {type: String, enum:['cakes', 'flowers', 'candles']},
+      productId: { type: String },
+      productName: { type: String },
+      productImage: { type: String },
+      price: { type: Number, required: true },
+      quantity: { type: Number }
+    }
+  ],
+  total: { type: Number },
+  status: { type: String, enum: ['pending', 'paid', 'processing', 'delivered', 'cancelled'] },
+  createdAt: { type: Date }
+})
+
+module.exports = orderSchema
--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
 const candleSchema = require('./candle')
+const orderSchema = require('./Order')
 
 const User = mongoose.model('User', userSchema)
 const Candle = mongoose.model('Candle', candleSchema)
+const Order = mongoose.model('Order', orderSchema)
 
 
 module.exports = {
   User,
-  Candle
+  Candle,
+  Order
 }
