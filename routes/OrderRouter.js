4e8481eee2e9ab2e9f2711ae4d137323b67b286e
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-22 20:24:42 +0300

    set up initial order router

---
+++ routes/OrderRouter.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const controller = require('../controllers/OrderController')
+const middleware = require('../middleware')
+
+router.get('/', controller.GetOrders)
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateOrder
+)
+
+router.put(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateOrder
+)
+
+router.delete(
+  ':/id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteOrder
+)
