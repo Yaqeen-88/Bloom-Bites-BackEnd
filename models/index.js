5f0a0b77b2503cd3abe364431b2049c50fc9b87d
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-23 19:15:27 +0300

    fixed models index

--- models/index.js
+++ models/index.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose')
 const userSchema = require('./User')
 const candleSchema = require('./candle')
 const flowerSchema = require('./Flower')
+const cakeSchema = require('./Cake')
 
 const User = mongoose.model('User', userSchema)
 const Candle = mongoose.model('Candle', candleSchema)
 const Flower = mongoose.model('Flower', flowerSchema)
+const Cake = mongoose.model('Cake', cakeSchema)
 
 
 module.exports = {
   User,
   Candle,
-  Flower
+  Flower,
+  Cake,
 }
