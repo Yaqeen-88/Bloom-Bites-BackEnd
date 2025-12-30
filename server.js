68f223cd3983e44db11cb0c02114b929124a914d
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2025-12-30 20:33:04 +0300

    fix capitlisation for deployment

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express")
+const logger = require("morgan")
+const cors = require("cors")
 
-const AuthRouter = require('./routes/AuthRouter')
-const CandleRouter = require('./routes/CandleRouter')
-const FlowerRouter = require('./routes/FlowerRouter')
-const CakeRouter = require('./routes/CakeRouter')
-const OrderRouter = require('./routes/OrderRouter')
+const AuthRouter = require("./routes/AuthRouter")
+const CandleRouter = require("./routes/CandleRouter")
+const FlowerRouter = require("./routes/FlowerRouter")
+const CakeRouter = require("./routes/CakeRouter")
+const OrderRouter = require("./routes/OrderRouter")
 
 const PORT = process.env.PORT || 3000
 
-const db = require('./db')
+const db = require("./db")
 
 const app = express()
 
 app.use(cors())
-app.use(logger('dev'))
+app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/auth', AuthRouter)
-app.use('/cake', CakeRouter)
-app.use('/candles', CandleRouter)
-app.use('/flowers',FlowerRouter)
-app.use('/orders', OrderRouter)
+app.use("/auth", AuthRouter)
+app.use("/cake", CakeRouter)
+app.use("/candles", CandleRouter)
+app.use("/flowers", FlowerRouter)
+app.use("/orders", OrderRouter)
 
-app.use('/', (req, res) => {
+app.use("/", (req, res) => {
   res.send(`Connected!`)
 })
 
 app.listen(PORT, () => {
-  console.log(`🔃 Running Express server on Port ${PORT} . . . `);
-
+  console.log(`🔃 Running Express server on Port ${PORT} . . . `)
 })
